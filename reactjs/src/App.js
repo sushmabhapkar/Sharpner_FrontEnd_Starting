d948fea844234b8f5199f91bc26c5e5b803ba414
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-02-24 17:38:54 +0530

    FrontEnd_start(expenseitem,description ,price)

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,10 +2,45 @@
  
  function App()
 {
+
+  const expenses=[
+
+    {
+      title:"Car  Insurance",
+      amount:4533,
+      date:new Date(2021,6,28)
+    },
+    {
+      title:"shopping cloths",
+      amount:6663,
+      date:new Date(2021,9,8)
+    },
+    {
+      title:"general store material",
+      amount:9333,
+      date:new Date(2021,6,28)
+    }
+
+  ];
   return (
     <div>
     <h2>....Expense Track Application.... </h2>
-    <ExpenseItem></ExpenseItem>
+    <ExpenseItem 
+    title={expenses[0].title}
+    amount={expenses[0].amount}
+    date={expenses[0].date}
+    ></ExpenseItem>
+    <ExpenseItem 
+    title={expenses[1].title}
+    amount={expenses[1].amount}
+    date={expenses[1].date}
+    ></ExpenseItem>
+    <ExpenseItem
+    title={expenses[2].title}
+    amount={expenses[2].amount}
+    date={expenses[2].date}
+    ></ExpenseItem>
+    
     </div>
   );
   
--- reactjs/src/component/ExpenseItem.js
+++ reactjs/src/component/ExpenseItem.js
@@ -1,18 +1,15 @@
 import "./ExpenseItem.css";
-function ExpenseItem()
-{
-  const ExpenseDate=new Date(2020,2,28);
-  const ExpenseTitle="Car Insurance";
-  const ExpensePrice=246.78;
 
-    return (
+function ExpenseItem(props)
+{
+   return (
         <div className="expense_item_date">
-         <h2>{ExpenseDate.toString()}</h2>
+         <h2>{props.date.toString()}</h2>
          <div className="expense_item_price">
-                <h2>${ExpensePrice}</h2>
+                <h2>${props.amount}</h2>
            <div className="expense_item_description">
-             <h2>{ExpenseTitle}</h2>
-             
+             <h2>{props.title}</h2>
+        
              </div>
            </div>
 
